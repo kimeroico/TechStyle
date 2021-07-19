7695b014bb414525312b693a712336532d99eca4
Kim Eroico <kim-eroico@hotmail.com>
2021-07-18 21:14:03 -0300

    New commit

--- TechStyle.Dominio/Repositorio/LojaRepositorio.cs
+++ TechStyle.Dominio/Repositorio/LojaRepositorio.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechStyle.Dominio.Modelo;
 
 namespace TechStyle.Dominio.Repositorio
 {
     public class LojaRepositorio
     {
+        private List<Loja> listaDaLoja;
+
+        public LojaRepositorio()
+        {
+            listaDaLoja = new List<Loja>();
+        }
+
+        public bool Incluir(Produto produto, int quantidadeLocal, int quantidadeMinima)
+        {
+            var produtoLoja = new Loja();
+            produtoLoja.Cadastrar(listaDaLoja.Count + 1, produto, quantidadeLocal, quantidadeMinima);
+
+            if (ValidandoDuplicidade(produtoLoja))
+            {
+                return false;
+            }
+
+            listaDaLoja.Add(produtoLoja);
+            return true;
+        }
+
+        private bool ValidandoDuplicidade(Loja loja)
+        {
+            return listaDaLoja.Any(x => x.Produto == loja.Produto);
+        }
+        
     }
 }
--- TechStyle.Dominio/Repositorio/ProdutoRepositorio.cs
+++ TechStyle.Dominio/Repositorio/ProdutoRepositorio.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechStyle.Dominio.Modelo;
 
 namespace TechStyle.Dominio.Repositorio
